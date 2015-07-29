a276ff37325ace0be23375850df4972847d1159a
Hyunseok <hyunseok@Hyunseok-ui-MacBook-Air.local>
2015-07-29 18:30:47 +0900

    Modify settings.py -- static root

--- django_bookmark/settings.py
+++ django_bookmark/settings.py
@@ -100,14 +100,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/bookmark/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_URL = '/static/'
